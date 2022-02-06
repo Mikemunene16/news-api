b23122653b57bc47b02fdb92d23d4ffbe55ddffd
Mike Munene <mike.munene@moringaschool.com>
2022-02-06 18:52:14 +0300

    Create app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
 
 
-# bootstrap = Bootstrap()
 
-# def create_app(config_name):
+def create_app(config_name):
 
-#     app = Flask(__name__)
+    app = Flask(__name__)
 
-#     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-#     # Will add the views and forms
+    # Will add the views and forms
 
-#     return app
+    return app
